5396c6c899dbe9b055831577689dacf639e98a42
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-28 10:15:21 -0700

    added a second test to account for a different method, test passing

--- tests/parseHTTPPacket.test.js
+++ tests/parseHTTPPacket.test.js
@@ -1,10 +1,17 @@
 const { parseHTTPPacket } = require('../utils/parseHTTPPacket');
 
 describe('testing makeHTTPPacket', () => {
-  it('returns both method and path', () => {
+  it('returns both method(GET) and path(/)', () => {
     const string = 'GET / ';
     const { method, path } = parseHTTPPacket(string);
     expect(method).toEqual('GET');
+    expect(path).toEqual('/');
+  });
+
+  it('returns both method(POST) and path(/)', () => {
+    const string = 'POST / ';
+    const { method, path } = parseHTTPPacket(string);
+    expect(method).toEqual('POST');
     expect(path).toEqual('/');
   });
 });
