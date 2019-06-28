24f92351452e350a74f1a5808e309359749b5570
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-28 10:32:48 -0700

    I changed data to request in my function, just to be a little better about naming that parameter

--- utils/parseHTTPPacket.js
+++ utils/parseHTTPPacket.js
@@ -1,5 +1,5 @@
-function parseHTTPPacket(data) {
-  const lines = data.toString().split('/n');
+function parseHTTPPacket(request) {
+  const lines = request.toString().split('/n');
 
   const method = lines[0].split(' ')[0];
   const path = lines[0].split(' ')[1];
