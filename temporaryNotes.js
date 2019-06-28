2dffb6399a13f526516ac0e5750062c5099dcda0
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-28 14:45:47 -0700

    I added another if statement and test to account for a POST method and a path of / and tests are passing

--- temporaryNotes.js
+++
@@ -1,5 +0,0 @@
-//what we thought was happening in the function is happening in our server
-//the only time we eed super test is in our server
-
-//response packet is different than request packet
-  //other function (parseHTTPPacket) was parsing the request packet
--- tests/app.test.js
+++ tests/app.test.js
@@ -2,12 +2,21 @@ const request = require('supertest');
 const { app } = require('../lib/app');
 
 describe('able to parse request and send response from server', () => {
-  it('able to parse request', () => {
+  it('able to parse request and send response for GET /', () => {
     return request(app)
       .get('/')
       .then(res => {
         expect(res.status).toEqual(200);
         expect(res.text).toEqual(expect.stringContaining('hi'));
+      });
+  });
+
+  it('able to parse request and send response for POST /', () => {
+    return request(app)
+      .post('/')
+      .then(res => {
+        expect(res.status).toEqual(400);
+        expect(res.text).toEqual(expect.stringContaining('sorry'));
       });
   });
 });
