63994b3fc624c895c4d2e823a7d1d7ab0b9f122d
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-28 13:44:02 -0700

    I imported both of my functions into my server

---
+++ tests/app.test.js
@@ -0,0 +1,8 @@
+const { request } = require('supertest');
+const { app } = require('../lib/app');
+
+describe('able to parse request and send response from server', () => {
+  it('able to parse request', () => {
+
+  });
+});
