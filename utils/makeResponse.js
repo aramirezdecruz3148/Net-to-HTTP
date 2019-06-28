4cf15277718994b2b1958caf1f18988d4140493a
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-28 13:16:23 -0700

    just created a a makeResponse function to return a response packet and tested it, tests are passing

---
+++ utils/makeResponse.js
@@ -0,0 +1,20 @@
+function makeResponse(statusCode, data, contentType) {
+  const date = new Date();
+
+  return `HTTP/1.1 ${statusCode}
+Date: ${date}
+Server: Apache
+Accept-Ranges: bytes
+Content-Length: Buffer.from${data.length}
+Content-Type: text/${contentType}
+
+${data}`;
+
+  //need to pass date in
+}
+
+module.exports = {
+  makeResponse
+};
+
+
